9f0de4a18791eca77b641443a6aa50d5e92b5981
Nicolle Girón <jackelyngironvillacinda@gmail.com>
2025-04-10 19:44:47 -0600

    Creacion de clases principales e implementacion de logica en pacientes y priority queue

---
+++ Paciente.java
@@ -0,0 +1,79 @@
+/*
+ * Clase Paciente
+ * Representa un paciente en sala de urgencias de un hospital
+ * 
+ * @author Jackelyn Giròn
+ */
+import java.time.LocalDateTime;
+public class Paciente implements Comparable<Paciente> {
+
+    private String nombre;
+    private String enfermedad;
+    private char codigo;
+    private LocalDateTime fechaIngreso;
+
+    /*
+     * Constructor de la clase Paciente
+     * @param nombre Nombre del paciente
+     * @param enfermedad Enfermedad del paciente
+     * @param codigo Codigo de prioridad del paciente
+     * @param fechaIngreso Fecha y hora de ingreso del paciente
+     */
+    public Paciente(String nombre, String enfermedad, char codigo, LocalDateTime fechaIngreso) {
+        this.nombre = nombre;
+        this.enfermedad = enfermedad;
+        this.codigo = codigo;
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    /*
+     * Getters y Setters de la clase Paciente
+     */
+    public String getNombre() {
+        return this.nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getEnfermedad() {
+        return this.enfermedad;
+    }
+    public void setEnfermedad(String enfermedad) {
+        this.enfermedad = enfermedad;
+    }
+    public char getCodigo() {
+        return this.codigo;
+    }
+    public void setCodigo(char codigo) {
+        this.codigo = codigo;
+    }
+    public LocalDateTime getFechaIngreso() {
+        return this.fechaIngreso;
+    }
+    public void setFechaIngreso(LocalDateTime fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    /*
+    * Método para comparar pacientes por su código de prioridad y fecha de ingreso
+     */
+    @Override
+    public int compareTo(Paciente urgencia) {
+        int priority = urgencia.getCodigo();
+        if (priority < this.codigo) {
+            return -1;
+        } else if (priority > this.codigo) {
+            return 1;
+        } else {
+            return this.fechaIngreso.compareTo(urgencia.getFechaIngreso());   
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "----------------------------" +
+                "nombre:'" + nombre + '\'' +
+                ", enfermedad:'" + enfermedad + '\'' +
+                ", codigo:" + codigo;
+    }
+}
---
+++ PriorityQueue.java
@@ -0,0 +1,12 @@
+/*
+ * Interface para PriorityQueue
+ * 
+ */
+public interface PriorityQueue<E extends Comparable<E>> {
+    public E getFirst();
+    public E remove();
+    public void add(E value);
+    public boolean isEmpty();
+    public int size();
+    public void clear();
+}
